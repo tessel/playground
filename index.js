2c4273d10e9e4014f65e2cef762f3741a5c249b7
Tim Cameron Ryan <id@timryan.org>
2015-03-09 15:32:15 -0700

    Adds PORT.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
 app.use(express.static('public'));
 app.use(express.static('build'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address == '::' ? 'localhost' : '';
   var port = server.address().port
 
